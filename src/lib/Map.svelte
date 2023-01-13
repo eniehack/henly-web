da877f7e6e5ed5a36f20043178f6e2d0d67c4b0a
Nakaya <eniehack@outlook.jp>
2023-01-14 03:43:49 +0900

    change way of importing leaflet.css

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,8 +1,8 @@
-<svelte:head>
+<!--<svelte:head>
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
    integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
    crossorigin=""/>
-</svelte:head>
+</svelte:head>-->
 
 <div id="map"></div>
 
@@ -15,6 +15,7 @@
 
 <script lang="ts">
  import L from "leaflet";
+ import 'leaflet/dist/leaflet.css';
  import type { Map as LFMap } from "leaflet";
  import { onMount, onDestroy, getContext } from "svelte";
  import type { Unsubscriber } from "svelte/store";
